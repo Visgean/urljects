208588d3315f071491310fe0328acb648245c472
Visgean Skeloru <visgean@gmail.com>
2015-07-18 15:41:19 +0200

    uuid4 and pk patterns

--- urljects/patterns.py
+++ urljects/patterns.py
@@ -1,6 +1,9 @@
 
-slug = r'(?P<slug>[\w-]+)'
 beginning = r'^'
 end = r'$'
+
+slug = r'(?P<slug>[\w-]+)'
+pk = '(?P<pk>\d+)'
+uuid4 = '(?P<uuid4>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})'  # noqa
 
 SEPARATOR = '/'  # separator for parts of the url
