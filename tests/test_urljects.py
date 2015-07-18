5ebb7588d2c1611a7d8576b9fd2b07b523eb6f67
Visgean Skeloru <visgean@gmail.com>
2015-07-18 15:18:46 +0200

    Singleton pattern that enabled U syntax

--- tests/test_urljects.py
+++ tests/test_urljects.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
 import unittest
 
 from urljects import U, slug
 
 
 class TestUrljects(unittest.TestCase):
-    def test_regulars(self):
-        test_data = [
-            [
-                r'^detail/(?P<slug>[\w-]+)',
-                U / 'detail' / slug
-            ]
-        ]
+    test_data = [
+        [
+            r'^detail/(?P<slug>[\w-]+)$',
+            U / 'detail' / slug
+        ],
+    ]
 
-        for old, new in test_data:
-            self.assertEqual(old, new.value)
+    def test_regulars(self):
+        for old, new in self.test_data:
+            self.assertEqual(old, new.get_value())
+
+    # def test_compile(self):
+    #     """
+    #     Tests that U object can actually compile to regex
+    #     """
+    #
+    #     re.compile(U / 'detail' / slug)
 
 
 if __name__ == '__main__':
--- urljects/urljects.py
+++ urljects/urljects.py
@@ -2,37 +2,56 @@ import re
 import patterns
 
 
-class UObject(object):
+class URLPattern(object):
     """
     This is the main urljects object, it is able to join strings and
     regular expressions. The value of this object will always be regular
     expression usable in django url.
     """
 
-    def __init__(self, value=patterns.beginning, separator=patterns.SEPARATOR):
+    def __init__(self, value=None, separator=patterns.SEPARATOR, ends=True):
+        """
+        :param value: Initial value of the URL
+        :param separator: used to separate parts of the url, usually /
+        :param ends: open urls should be used only for included urls
+        """
         self.value = value
         self.separator = separator
+        self.ends = ends
 
     def add_part(self, part):
         """
         Function for adding partial pattern to the value
         :param part: string or compiled pattern
         """
-
         if isinstance(part, re._pattern_type):
             part = part.pattern
 
-        if self.value == patterns.beginning:
-            self.value += part
+        if self.value is None:
+            return URLPattern(value=part)
         else:
             self.value += self.separator + part
         return self
 
+    def get_value(self):
+        """
+        This function finishes the url pattern creation by adding starting
+        character ^ end possibly by adding end character at the end
+        :return: raw string
+        """
+        value = self.value
+        if self.value[0] != patterns.beginning:
+            value = patterns.beginning + self.value
+
+        if self.ends and self.value[-1] != patterns.end:
+            value += patterns.end
+        return value
+
     def __div__(self, other):
-        self.add_part(other)
-        return self
+        return self.add_part(other)
 
     def __repr__(self):
-        return self.value
+        return self.get_value() or ''
 
-U = UObject()
+
+U = URLPattern()
