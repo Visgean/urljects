87a9f3256880293f90bdc320e4885018faddafae
Visgean Skeloru <visgean@gmail.com>
2015-07-19 17:06:37 +0200

    namedtuple for tests

--- tests/test_urljects.py
+++ tests/test_urljects.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from collections import namedtuple
 
 import re
 import unittest
@@ -7,35 +8,46 @@ import unittest
 from urljects import I, U, slug
 
 
-class TestUrljects(unittest.TestCase):
+URLTest = namedtuple('URLTest', ['old_url', 'new_url', 'view', 'name'])
+
+
+class TestURLjects(unittest.TestCase):
     test_data = [
-        [
-            r'^detail/(?P<slug>[\w-]+)$',
-            U / 'detail' / slug
-        ],
-        [
-            r'^(?P<slug>[\w-]+)$',
-            U / slug
-        ],
-        [
-            r'^detail$',
-            U / 'detail'
-        ],
-        [
-            r'^products',
-            I / 'products'
-        ],
+        URLTest(
+            old_url=r'^detail/(?P<slug>[\w-]+)$',
+            new_url=U / 'detail' / slug,
+            view=None,
+            name=None
+        ),
+        URLTest(
+            old_url=r'^(?P<slug>[\w-]+)$',
+            new_url=U / slug,
+            view=None,
+            name=None
+        ),
+        URLTest(
+            old_url=r'^static$',
+            new_url=U / 'static',
+            view=None,
+            name=None
+                ),
+        URLTest(
+            old_url=r'^products',
+            new_url=I / 'products',
+            view=None,
+            name=None
+        ),
     ]
 
     def test_regulars(self):
-        for old, new in self.test_data:
-            self.assertEqual(old, new.get_value())
+        for url_test in self.test_data:
+            self.assertEqual(url_test.old_url, url_test.new_url.get_value())
 
     def test_compile(self):
         """
         Tests that U object can actually compile to regex
         """
-        patterns_to_compile = (g[1] for g in self.test_data)
+        patterns_to_compile = (g.new_url for g in self.test_data)
         for pattern in patterns_to_compile:
             re.compile(pattern.get_value())
 
