9a83dc480e27e07d64a07d2bea038ac0e42f86d3
Visgean Skeloru <visgean@gmail.com>
2015-07-24 23:36:15 +0200

    Untested view_include and py3 support
    URLView class - support for CBV
    url_view decorator - support for FBV
    
    view_include - support for shit above
    
    six put into use, so far its untested

--- urljects/urljects.py
+++ urljects/urljects.py
@@ -1,5 +1,8 @@
 import re
 import patterns
+import six
+import inspect
+import functools
 
 from django.conf import urls
 
@@ -67,17 +70,68 @@ U = URLPattern()
 I = URLPattern(ends=False)
 
 
+class URLView(object):
+    """
+        Abstract class with url and name.
+
+        Since URLs can't be changed on runtime it is kind of pointless
+        to have ``get_url`` or ``get_url_name`` methods. So there are not any.
+    """
+
+    url = NotImplemented
+    url_name = NotImplemented
+
+
+def url_view(url_pattern, name=None):
+    """
+    Decorator for registering functional views.
+    Meta decorator syntax has to be used in order to accept arguments.
+
+    This decorator does not really do anything that magical, you could achieve
+
+    This:
+    >>> @url_view(U / 'my_view')
+    ... def my_view(request)
+    ...     pass
+
+    is equivalent to this:
+    >>> def my_view(request)
+    ...     pass
+    >>> my_view.urljects_view = True
+    >>> my_view.url = U / 'my_view'
+    >>> my_view.url_name = 'my_view'
+
+    Those view are then supposed to be used with ``view_include`` which will
+    register all views that have ``urljects_view`` set to ``True``.
+
+    :param url_pattern: regex or URLPattern or anything passable to url()
+    :param name: name of the view, func_name will be used otherwise.
+    """
+
+    def meta_wrapper(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        wrapper.urljects_view = True
+        wrapper.url = url_pattern
+        wrapper.url_name = name or func.func_name
+
+        return wrapper
+    return meta_wrapper
+
+
 def resolve_name(view):
     """
     Auto guesses name of the view.
     For function it will be ``view.func_name``
-    For classes it will be ``view.name``
+    For classes it will be ``view.url_name``
     """
     if hasattr(view, 'func_name'):
         return view.func_name
-    if hasattr(view, 'name'):
-        return view.name
-    if isinstance(view, basestring):
+    if hasattr(view, 'url_name'):
+        return view.url_name
+    if isinstance(view, six.string_types):
         return view.split('.')[-1]
     return None
 
@@ -114,9 +168,33 @@ def url(regex, view, kwargs=None, name=None, prefix=''):
         prefix=prefix)
 
 
-def view_include(view):
+def view_include(view_module, namespace=None, app_name=None):
+    """
+    Includes view in the url, works similar to django include function.
+    Auto imports all class based views that are subclass of ``URLView`` and
+    all functional views that have been decorated with ``url_view``.
+
+    :param view_module: object of the module or string with importable path
+    :param namespace: name of the namespaces, it will be guessed otherwise
+    :param app_name: application name
+    :return: result of url include
     """
 
-    :param view:
-    :return:
-    """
+    # since Django 1.8 patterns() are deprecated, list should be used instead
+    view_patterns = []
+
+    if isinstance(view_module, six.string_types):
+        view_module = __import__(view_module)
+
+    for member in inspect.getmembers(view_module):
+        is_class_view = inspect.isclass(member) and issubclass(member, URLView)
+        is_func_view = (inspect.isfunction(member)
+                        and hasattr(member, 'urljects_view')
+                        and member.urljects_view)
+        if is_class_view or is_func_view:
+            view_patterns.append(url(member.url, member, name=member.url_name))
+
+    return urls.include(
+        arg=view_patterns,
+        namespace=namespace,
+        app_name=app_name)
