87e69ea38b12e857f3b8d8c5e993e08d38bd8b91
Tomas Peterka <prestizni@gmail.com>
2017-01-30 15:45:22 +0100

    Add new pattern "rest" which ... well matches the rest of URL

--- urljects/patterns.py
+++ urljects/patterns.py
@@ -6,6 +6,7 @@ end = r'$'
 slug = r'(?P<slug>[\w-]+)'
 pk = '(?P<pk>\d+)'
 uuid4 = '(?P<uuid4>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})'  # noqa
+rest = '(?P<rest>[\w\-\_\.\@\:/]*)'  # match anything acceptable in URL
 
 year = r'(?P<year>\d{4})'
 month = r'(?P<month>0?([1-9])|10|11|12)'
